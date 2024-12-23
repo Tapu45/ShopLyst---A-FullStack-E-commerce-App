fae076762d5cfa4d422557a554da5943c1f5c4dd
Tapu45 <rameswarpanda891@gmail.com>
2024-12-23 23:48:27 +0530

    app.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage"
-import SignupPage from "./pages/SignupPage"
+import SignUpPage from "./pages/SignUpPage"
 import LoginPage from "./pages/LoginPage"
 import AdminPage from "./pages/AdminPage"
 import CategoryPage from "./pages/CategoryPage"
@@ -43,7 +43,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path='/signup' element={!user ? <SignupPage /> : <Navigate to='/' />} />
+        <Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to='/' />} />
 					<Route path='/login' element={!user ? <LoginPage /> : <Navigate to='/' />} />
           <Route path='secret-dashboard' element={user?.role === "admin" ? <AdminPage /> : <Navigate to='/login' />} />
           <Route path='/category/:category' element={<CategoryPage />} />
